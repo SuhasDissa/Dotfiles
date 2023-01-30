2b31c82980c25415a6341b167a94011a3fd1be0e
Suhas Dissanayake <suhasdissa@protonmail.com>
2023-01-31 02:39:53 +0530

    Initial commit

---
+++ create_links.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ln -s ~/Dotfiles/fish ~/.config/
+ln -s ~/Dotfiles/hypr ~/.config/
+ln -s ~/Dotfiles/kitty ~/.config/
+ln -s ~/Dotfiles/rofi ~/.config/
+ln -s ~/Dotfiles/waybar ~/.config/
+ln -s ~/Dotfiles/wlogout ~/.config/
+ln -s ~/Dotfiles/fontconfig ~/.config/
+ln -s ~/Dotfiles/dunst ~/.config/
---
+++ fish/functions/injector.fish
@@ -0,0 +1,4 @@
+function injector
+    cd ~/Documents/HTTP/
+    ./http_Injector
+end
